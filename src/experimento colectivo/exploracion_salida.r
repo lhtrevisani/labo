fd8da68c1c753766160393964448d8c556a71e80
lucastrevisani <86080534+lucastrevisani@users.noreply.github.com>
2022-10-23 21:41:13 -0300

    experimento colectivo

--- src/experimento colectivo/exploracion_salida.r
+++ src/experimento colectivo/exploracion_salida.r
@@ -1,9 +1,3 @@
-#Necesita para correr en Google Cloud
-# 128 GB de memoria RAM
-# 256 GB de espacio en el disco local
-#   8 vCPU
-
-
 #limpio la memoria
 rm( list=ls() )  #remove all objects
 gc()             #garbage collection
@@ -37,17 +31,17 @@ tb_log_wfv$ganancia_max_acum = cummax(tb_log_wfv$ganancia)
 # leo las ganancias en test (cross validaition)
 arch_log  <- paste0( base_dir, "exp/", PARAM$experimento_cv, "/ganancias_iteracion.csv" )
 ganancias_cv  <- fread( arch_log )
-colnames(ganancias_cv) <- c("iteracion_bayesiana", "ganancia_test")
+colnames(ganancias_cv) <- c("iteracion_bayesiana", "ganancia_test", "ganancia_test_corte")
 
 # leo las ganancias en test (walk forward validation)
 arch_log  <- paste0( base_dir, "exp/", PARAM$experimento_wfv, "/ganancias_iteracion.csv" )
 ganancias_wfv  <- fread( arch_log )
-colnames(ganancias_wfv) <- c("iteracion_bayesiana", "ganancia_test")
+colnames(ganancias_wfv) <- c("iteracion_bayesiana", "ganancia_test", "ganancia_test_corte")
 
 ## corrijo la fecha de la iteración para contemplar el rato en el que se había caído la virtual machine, y parseo las fechas.
 tb_log_cv = tb_log_cv %>%
-  mutate(fecha_parsed = case_when(ymd_hms(fecha) > as.POSIXct("2022-10-21 16:00:00") ~ ymd_hms(fecha)- minutes(45),
-                                    TRUE ~ ymd_hms(fecha)),
+  mutate(#fecha_parsed = ymd_hms(fecha),
+         fecha_parsed = case_when(ymd_hms(fecha) > as.POSIXct("2022-10-21 16:00:00") ~ ymd_hms(fecha)- minutes(45), TRUE ~ ymd_hms(fecha)),
          cumulative_time = difftime(fecha_parsed, min(fecha_parsed), units='mins'),
          iteration_duration = as.numeric(difftime(fecha_parsed, lag(fecha_parsed), units='mins'))) %>%
   select(iteracion_bayesiana, fecha_parsed, num_iterations, learning_rate, feature_fraction, min_data_in_leaf, num_leaves, prob_corte, ganancia, ganancia_max_acum, cumulative_time, iteration_duration)
@@ -57,7 +51,8 @@ tb_log_wfv = tb_log_wfv %>%
   mutate(fecha_parsed = ymd_hms(fecha),
          cumulative_time = difftime(fecha_parsed, min(fecha_parsed), units='mins'),
          iteration_duration = as.numeric(difftime(fecha_parsed, lag(fecha_parsed), units='mins'))) %>%
-  select(iteracion_bayesiana, fecha_parsed, num_iterations, learning_rate, feature_fraction, min_data_in_leaf, num_leaves, prob_corte, ganancia, ganancia_max_acum, cumulative_time, iteration_duration)
+  select(iteracion_bayesiana, fecha_parsed, num_iterations, learning_rate, feature_fraction, min_data_in_leaf, num_leaves, prob_corte, ganancia, ganancia_max_acum, cumulative_time, iteration_duration) %>%
+  filter(iteracion_bayesiana < 148)
 
 tb_log_cv = left_join(tb_log_cv, ganancias_cv, by = "iteracion_bayesiana")
 tb_log_wfv = left_join(tb_log_wfv, ganancias_wfv, by = "iteracion_bayesiana")
@@ -65,3 +60,55 @@ tb_log_wfv = left_join(tb_log_wfv, ganancias_wfv, by = "iteracion_bayesiana")
 
 rm(ganancias_cv)
 rm(ganancias_wfv)
+
+
+## cuál es la ganancia máxima a la que llegó cada uno de los modelos:
+
+# cross validation:
+tb_log_cv %>% tail(1) %>% select(ganancia_test_corte)
+
+# walk forward validation:
+tb_log_wfv %>% tail(1) %>% select(ganancia_test_corte)
+
+
+## cuáles son los hiperparámetros de la mejor optimización bayesiana:
+
+# cross validation:
+tb_log_cv %>% tail(1) %>% select(num_iterations, learning_rate, feature_fraction, min_data_in_leaf, num_leaves)
+
+# walk forward validation:
+tb_log_wfv %>% tail(1) %>% select(num_iterations, learning_rate, feature_fraction, min_data_in_leaf, num_leaves)
+
+
+## uno los dos datasets:
+log_completo = rbind(tb_log_cv %>% mutate(modelo = 'cv'), tb_log_wfv %>% mutate(modelo = 'wfv'))
+
+
+## tiempo promedio de la iteración:
+log_completo %>%
+  group_by(modelo) %>%
+  summarise(iteracion_promedio = mean(iteration_duration, na.rm = TRUE))
+
+log_ganancias = log_completo %>% filter(!is.na(ganancia_test_corte))
+  
+
+library(ggplot2)
+library(hrbrthemes)
+
+ggplot(log_ganancias,                            # Draw ggplot2 time series plot
+       aes(x = cumulative_time,
+           y = ganancia_test_corte,
+           col = modelo)) +
+  geom_line() + 
+  geom_point() +
+  geom_text(aes(label = iteracion_bayesiana), check_overlap = T, size = 3, nudge_y = -50000) +
+  geom_text(aes(label = paste0("$",round(ganancia_test_corte/1000000, 2), "M")), check_overlap = T, nudge_y = 50000, size = 3) + 
+  theme_ipsum() + 
+  labs(title="Ganancia en test y cantidad de iteraciones",
+       x ="Minutos desde la primera iteración", 
+       y = "Ganancia en test",
+       colour = "Validación"
+       ) +
+  scale_x_continuous(breaks = seq(0, 320, 50))
+
+  
