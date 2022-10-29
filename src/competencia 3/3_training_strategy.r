9815ab592d0165c10b562df5625a3c295fe3b0c8
lucastrevisani <86080534+lucastrevisani@users.noreply.github.com>
2022-10-29 11:16:50 -0300

    subm3

--- src/competencia 3/3_training_strategy.r
+++ src/competencia 3/3_training_strategy.r
@@ -14,15 +14,15 @@ require("data.table")
 
 #Parametros del script
 PARAM  <- list()
-PARAM$experimento <- "TS9311"
+PARAM$experimento <- "TS9312"
 
-PARAM$exp_input  <- "FE9250"
+PARAM$exp_input  <- "FE9251"
 
 PARAM$future       <- c( 202107 )
 
-PARAM$final_train  <- c( 202101, 202102, 202103, 202104, 202105 )
+PARAM$final_train  <- c( 202012, 202101, 202102, 202103, 202104, 202105 )
 
-PARAM$train$training     <- c( 202101, 202102, 202103 )
+PARAM$train$training     <- c( 202010, 202011, 202012, 202101, 202102, 202103 )
 PARAM$train$validation   <- c( 202104 )
 PARAM$train$testing      <- c( 202105 )
 PARAM$train$undersampling  <- 1.0   # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
